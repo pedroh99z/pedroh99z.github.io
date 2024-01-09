c22795843d5e782a1c6e5a21bfccad2ff756b5a2
pedroh99z <pedroh99_@hotmail.com>
2024-01-09 15:42:21 -0300

    posts

---
+++ load_content.js
@@ -0,0 +1,18 @@
+		current_url = window.location.href
+		if (current_url.includes("https://pedroh99z.github.io/?post=")){
+		    current_url = current_url.split("https://pedroh99z.github.io/?post=")
+		    current_url = current_url[1]
+		    eval('document.write("<h1>" + window.POST_'+current_url+'[0] + "</h2>")')
+		    eval('document.write("<h3>" + window.POST_'+current_url+'[1] + "</h3>")')
+		    eval('document.write(window.POST_'+current_url+'[2])')
+		}
+		else {
+		
+		document.write("<h1>" + window.POST_hello_world[0] + "</h2>")
+		document.write("<h3>" + window.POST_hello_world[1] + "</h3>")
+		document.write(window.POST_hello_world[2])
+		
+		document.write("<h1>" + window.POST_lorem_ipsum[0] + "</h2>")
+		document.write("<h3>" + window.POST_lorem_ipsum[1] + "</h3>")
+		document.write(window.POST_lorem_ipsum[2])
+		}
