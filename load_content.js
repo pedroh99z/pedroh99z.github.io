08b62b9cdf0f0269cac32cfc3acf92bd8fc5e97f
pedroh99z <pedroh99_@hotmail.com>
2024-01-09 15:50:14 -0300

    posts

--- load_content.js
+++ load_content.js
@@ -1,10 +1,15 @@
 		current_url = window.location.href
 		if (current_url.includes("https://pedroh99z.github.io/?post=")){
+		    try {
 		    current_url = current_url.split("https://pedroh99z.github.io/?post=")
 		    current_url = current_url[1]
 		    eval('document.write("<h1>" + window.POST_'+current_url+'[0] + "</h2>")')
 		    eval('document.write("<h3>" + window.POST_'+current_url+'[1] + "</h3>")')
 		    eval('document.write(window.POST_'+current_url+'[2])')
+		    }
+		    catch (e) {
+		    document.write("<h2> Erro 404 </h2> </br> <h3> Página não encontrada! </h3>")
+		    }
 		}
 		else {
 		
