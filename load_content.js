21802a646c057416bfd5a6fab733f083035eedbe
pedroh99z <pedroh99_@hotmail.com>
2024-08-08 18:54:17 -0300

    gitacp

--- load_content.js
+++ load_content.js
@@ -3,10 +3,16 @@
 		    try {
 		    current_url = current_url.split("https://pedroh99z.github.io/?post=")
 		    current_url = current_url[1]
+          if (current_url = "sidebar") {
+            eval('document.write(window.POST_'+current_url+'[2])')
+          }
+          else
+          {
 		    eval('document.write("<h1>" + window.POST_'+current_url+'[0] + "</h2>")')
 		    eval('document.write("<h3>" + window.POST_'+current_url+'[1] + "</h3>")')
 		    eval('document.write(window.POST_'+current_url+'[2])')
 		    }
+        }
 		    catch (e) {
 		    document.write("<h2> Erro 404 </h2> <h3> Página não encontrada! </h3>")
 		    }
