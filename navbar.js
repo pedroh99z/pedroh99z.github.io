06a4f5f831bd07cc67d3ca55d34156c245e560b3
pedroh99z <pedroh99_@hotmail.com>
2024-08-08 17:42:08 -0300

    gitacp

---
+++ navbar.js
@@ -0,0 +1,7 @@
+var NAVBAR_options = [["Home2", "https://pedroh99z.github.io/"],
+["Notícias", "#"],
+["Contato", "#"]]
+
+var path = window.location.pathname;
+var page = path.split("/").pop();
+alert( page );
