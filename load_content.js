084ba2c8a7d697600820d94d4ed522d13a8ea247
pedroh99z <pedroh99_@hotmail.com>
2024-08-11 14:53:08 -0300

    gitacp

--- load_content.js
+++ load_content.js
@@ -1,18 +1,37 @@
-		current_url = window.location.href
+function show_full_post(post_alias) {
+    eval('document.write("<h1>" + POST_'+post_alias+'.title + "</h2>")')
+    eval('document.write("<h3>" + POST_'+post_alias+'.date + "</h3>")')
+    eval('document.write(POST_'+post_alias+'.content)')
+    eval('document.write("<br><br><b>Palavras-chave:</b> " + POST_'+post_alias+'.keywords)')
+
+}
+
+function show_post_abstract(post_alias) {
+		eval('document.write("<h1>" + POST_'+post_alias+'.title + "</h2>")');
+		eval('document.write("<h3>" + POST_'+post_alias+'.date + "</h3>")');
+		eval('document.write(POST_'+post_alias+'.description)');
+    str = "'?post="+post_alias+"'";
+    eval('document.write("<br><br><a href='+str+' >Ver mais... </a>")');
+
+}
+
+function show_post_prop(post_alias, prop) {
+  // this shows the specified post property without any formatting.
+		eval('document.write(POST_'+post_alias+'.'+prop+')');
+}
+
+current_url = window.location.href
 		if (current_url.includes("?post=")){
 		    try {
 		    current_url = current_url.split("?post=")
 		    current_url = current_url[1]
           if (current_url == "sidebar") {
-            eval('document.write(POST_'+current_url+'.content)')
+            show_post_prop(current_url, "content")
           }
           else
           {
-		    eval('document.write("<h1>" + POST_'+current_url+'.title + "</h2>")')
-		    eval('document.write("<h3>" + POST_'+current_url+'.date + "</h3>")')
-		    eval('document.write(POST_'+current_url+'.content)')
-		    eval('document.write("<br><br>Palavras-chave: " + POST_'+current_url+'.keywords)')
-		    }
+            show_full_post(current_url)
+          }
         }
 		    catch (e) {
 		    document.write("<h2> Erro 404 </h2> <h3> Página não encontrada! </h3>")
@@ -20,14 +39,7 @@
 		}
 		else {
 	
-		document.write("<h1>" + POST_hello_world.title + "</h2>")
-		document.write("<h3>" + POST_hello_world.date + "</h3>")
-		document.write(POST_hello_world.description)
-    document.write("<br><a href='?post=hello_world' >Ver mais... </a>")
-		
-		document.write("<h1>" + POST_lorem_ipsum.title + "</h2>")
-		document.write("<h3>" + POST_lorem_ipsum.date + "</h3>")
-		document.write(POST_lorem_ipsum.description)
-    document.write("<br><a href='?post=lorem_ipsum' >Ver mais... </a>")
+      show_post_abstract("hello_world")
+      show_post_abstract("lorem_ipsum")
 
 		}
